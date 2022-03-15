9fcea05a9334443aaa624d73d73044287197181c
yakuptepetam <101400116+yakuptepetam@users.noreply.github.com>
2022-03-15 23:20:40 +0300

    Add files via upload

---
+++ Patika_Odev_Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Dictionary
+        Dictionary<int, string> dic = new Dictionary<int, string>();
+        dic.Add(1, "Yakup");
+        dic.Add(2, "Tepetam");
+
+        //Elemanlara Erişim
+        foreach (int key in dic.Keys)
+        {
+            Console.WriteLine(key);
+        }
+
+        //Count
+        Console.WriteLine(dic.Count);
+
+        //Contains
+        Console.WriteLine(dic.ContainsValue("Yakup"));
+        Console.WriteLine(dic.ContainsKey(2));
+
+        //Remove
+        dic.Remove(2);
+    }
+}
