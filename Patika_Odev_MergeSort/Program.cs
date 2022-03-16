e903f728cab4b88321e2829c923354a02e5ff512
yakuptepetam <101400116+yakuptepetam@users.noreply.github.com>
2022-03-16 23:09:14 +0300

    Add files via upload

---
+++ Patika_Odev_MergeSort/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Merge Sort Ödevi");
+        Console.WriteLine();
+        Console.WriteLine("Dizi: [16,21,11,8,12,22]");
+        Console.WriteLine();
+        Console.WriteLine("1. Yukarı verilen dizinin sort türüne göre aşamalarını yazınız.");
+        Console.WriteLine();
+        Console.WriteLine("                             [16,21,11,8,12,22]");
+        Console.WriteLine(@"                            /                \");
+        Console.WriteLine("                   [16,21,11]                 [8,12,22]");
+        Console.WriteLine(@"                    /     \                   /     \");
+        Console.WriteLine(@"               [16,21]    [11]           [8,12]    [22]");
+        Console.WriteLine(@"                /   \      |             /   \      |");
+        Console.WriteLine("             [16]   [21]  [11]          [8]   [12]  [22]");
+        Console.WriteLine(@"              \    /       |            \    /       |");
+        Console.WriteLine(@"               [16,21]    [11]           [8,12]    [22]");
+        Console.WriteLine(@"                   \       /                \       /");
+        Console.WriteLine(@"                  [11,16,21]                [8,12,22]");
+        Console.WriteLine(@"                          \                  /");
+        Console.WriteLine("                            [8,11,12,16,21,22]");
+        Console.WriteLine();
+        Console.WriteLine("2. Big-O gösterimini yazınız.");
+        Console.WriteLine();
+        Console.WriteLine("O(nlogn)");
+    }
+}
