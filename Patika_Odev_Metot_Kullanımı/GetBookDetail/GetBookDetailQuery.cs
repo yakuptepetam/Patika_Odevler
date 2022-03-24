40a62e45f49e5d65003dddaa64d31f65ae2061a8
yakuptepetam <101400116+yakuptepetam@users.noreply.github.com>
2022-03-24 16:40:46 +0300

    Add files via upload

---
+++ Patika_Odev_Metot_Kullanımı/GetBookDetail/GetBookDetailQuery.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Common;
+using WebApi.DBOperations;
+
+namespace WebApi.BookOperations.GetBookDetail
+{
+    public class GetBookDetailQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+        public int BookId { get; set; }
+        public GetBookDetailQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public BookDetailViewModel Handle()
+        {
+            var book = _dbContext.Books.Where(book=> book.Id == BookId).SingleOrDefault();
+            if(book is null) // book != null
+            {
+                throw new InvalidOperationException("Kitap bulunamadı");
+            }
+            else
+            {
+                BookDetailViewModel vm = new BookDetailViewModel();
+                vm.Title = book.Title;
+                vm.Genre = ((GenreEnum)book.GenreId).ToString();
+                vm.PageCount = book.PageCount;
+                vm.PublishDate = book.PublishDate.Date.ToString("dd/MM/yyyy");
+                return vm;
+            }
+        }
+
+        public class BookDetailViewModel
+        {
+            public string Title { get; set; }
+            public string Genre { get; set; }
+            public int PageCount { get; set; }
+            public string PublishDate { get; set; }
+        }
+    }
+}
---
+++ Patika_Odev_Metot_Kullanımı/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Common;
+using WebApi.DBOperations;
+
+namespace WebApi.BookOperations.UpdateBook
+{
+    public class UpdateBookCommand
+    {
+        public UpdateBookModel Model { get; set; }
+        private readonly BookStoreDbContext _dbContext;
+        public int BookId { get; set; }
+        public UpdateBookCommand(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Handle()
+        {
+            var book = _dbContext.Books.SingleOrDefault(x=>x.Id == BookId);
+            if(book is null)
+            {
+                throw new InvalidOperationException("Kitap bulunamadı");
+            }
+            else
+            {
+                book.GenreId = Model.GenreId != default ? Model.GenreId : book.GenreId;
+                book.PageCount = Model.PageCount != default ? Model.PageCount : book.PageCount;
+                book.PublishDate = Model.PublishDate != default ? Model.PublishDate : book.PublishDate;
+                book.Title = Model.Title != default ? Model.Title : book.Title;
+                _dbContext.SaveChanges();
+            }
+        }
+        public class UpdateBookModel
+        {
+            public string Title { get; set; }
+            public int GenreId { get; set; }
+            public int PageCount { get; set; }
+            public DateTime PublishDate { get; set; }
+        }
+    }
+}
